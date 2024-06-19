2b13cdacc62dc898ebc16da43e3c8724e08f1085
Bmack1915 <119226403+Bmack1915@users.noreply.github.com>
2024-06-19 15:24:17 +0100

    FetchedWainwrights

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import axios from "axios";
 
 function App() {
   const [ErrorMsg, setErrorMsg] = useState("");
+  const [wainwrights, setWainwrights] = useState([]);
   useEffect(() => {
     const fetchDataTest = async () => {
       try {
         const res = await axios.get(`${API_BASE_URL}Wainwrights`);
-        const data = res.json();
-        console.log(data);
+        const Wainwrights = res.data;
+        console.log(Wainwrights);
+        setWainwrights(Wainwrights);
       } catch (err) {
         setErrorMsg("Error found");
         console.log(ErrorMsg);
@@ -18,12 +20,25 @@ function App() {
     };
 
     fetchDataTest();
-  }, [ErrorMsg]);
+  }, []);
 
   return (
     <div>
       <p>Hello Ben</p>
-      {ErrorMsg && <p>{ErrorMsg}</p>}
+      {wainwrights.map((w) => (
+        <Wainwright w={w} />
+      ))}
+    </div>
+  );
+}
+
+function Wainwright({ w }) {
+  return (
+    <div>
+      <p>
+        {w.name} is {w.heightM}m / {w.heightFeet} tall and is ranked{" "}
+        {w.rankByHeight} by height. Grid Ref: {w.osGridRef}
+      </p>
     </div>
   );
 }
