330324ea21a90b907d0680ac2176030c0dce6798
Bmack1915 <119226403+Bmack1915@users.noreply.github.com>
2024-06-21 08:27:27 +0100

    InitialMap w/ Markers added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,146 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import { API_BASE_URL } from "./apiConfig";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
+import {
+  APIProvider,
+  AdvancedMarker,
+  InfoWindow,
+  Map,
+  Pin,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
+import { API_BASE_URL, API_KEY } from "./apiConfig";
+import "./App.css";
 
 function App() {
-  const [ErrorMsg, setErrorMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
   const [wainwrights, setWainwrights] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    const fetchDataTest = async () => {
+    setIsLoading(true);
+    const fetchData = async () => {
       try {
         const res = await axios.get(`${API_BASE_URL}Wainwrights`);
-        const Wainwrights = res.data;
-        console.log(Wainwrights);
-        setWainwrights(Wainwrights);
+        setWainwrights(res.data);
+        console.log(wainwrights);
       } catch (err) {
         setErrorMsg("Error found");
-        console.log(ErrorMsg);
+        console.error("Error fetching data:", err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
-    fetchDataTest();
+    fetchData();
   }, []);
 
   return (
     <div>
-      <p>Hello Ben</p>
-      {wainwrights.map((w) => (
-        <Wainwright w={w} />
-      ))}
+      <p>Test</p>
+
+      {!isLoading && wainwrights.length > 0 ? (
+        <MapSummary wainwrights={wainwrights} />
+      ) : (
+        <Loading />
+      )}
+      {errorMsg && <p>{errorMsg}</p>}
+      <p>Map should be here</p>
     </div>
   );
 }
 
-function Wainwright({ w }) {
+function Loading() {
+  return <p>🕣 Page is loading...</p>;
+}
+function MapSummary({ wainwrights }) {
+  const [selected, setSelected] = useState(null);
+
+  const containerStyle = {
+    width: "500px",
+    height: "500px",
+  };
+
+  function handleMapClick() {
+    setSelected(null);
+  }
+
   return (
-    <div>
-      <p>
-        {w.name} is {w.heightM}m / {w.heightFeet} tall and is ranked{" "}
-        {w.rankByHeight} by height. Grid Ref: {w.osGridRef}
-      </p>
-    </div>
+    <APIProvider apiKey={API_KEY}>
+      <Map
+        onClick={handleMapClick}
+        mapId={"2bf56b659e5bf4cc"}
+        style={containerStyle}
+        defaultCenter={{
+          lat: wainwrights[0].latitude,
+          lng: wainwrights[0].longitude,
+        }}
+        defaultZoom={9}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
+      >
+        {wainwrights.map((w) => (
+          <MarkerWithInfoWindow
+            setSelected={setSelected}
+            w={w}
+            key={w.wainwrightID}
+            selected={selected}
+          />
+        ))}
+      </Map>
+    </APIProvider>
   );
 }
+
+const MarkerWithInfoWindow = ({ w, setSelected, selected }) => {
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const infoWindowShown = selected === w.wainwrightID;
+
+  const areaColors = {
+    Southern: "red",
+    Northern: "blue",
+    Eastern: "green",
+    Western: "orange",
+    Central: "purple",
+  };
+
+  function getBackgroundColour(area) {
+    return areaColors[`${area} `];
+  }
+
+  const background = getBackgroundColour(w.area + " ");
+
+  function handleMarkerClick() {
+    setSelected(w.wainwrightID);
+  }
+
+  // if the maps api closes the infowindow, we have to synchronize our state
+  const handleClose = useCallback(() => setSelected(null), []);
+
+  return (
+    <>
+      <AdvancedMarker
+        ref={markerRef}
+        onClick={handleMarkerClick}
+        position={{
+          lat: w.latitude,
+          lng: w.longitude,
+        }}
+      >
+        <Pin
+          background={background}
+          borderColor={"black"}
+          glyphColor={"black"}
+        ></Pin>
+      </AdvancedMarker>
+
+      {infoWindowShown && (
+        <InfoWindow key={w.wainwrightID} anchor={marker} onClose={handleClose}>
+          <h2>{w.name}</h2>
+          <p>Click here to find out more!</p>
+        </InfoWindow>
+      )}
+    </>
+  );
+};
 
 export default App;
--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,3 @@
 // src/apiConfig.js
 export const API_BASE_URL = "http://localhost:5286/api/";
+export const API_KEY = "AIzaSyDKWZbZ93fuajxbQukXEy7N-nFEgdTKu-g";
